e374e5687a7fd19968df0617c79e0532cd724657
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2017-04-15 19:11:54 +0200

    Created Spotlight Importer target

---
+++ FLIFSpotlightImporter/FLIFSpotlightImporter.h
@@ -0,0 +1,21 @@
+//
+//  MySpotlightImporter.h
+//  FLIFSpotlightImporter
+//
+//  Created by Sveinbjorn Thordarson on 15/04/2017.
+//  Copyright © 2017 Sveinbjorn Thordarson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#define YOUR_STORE_TYPE NSXMLStoreType
+
+@interface FLIFSpotlightImporter : NSObject
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (BOOL)importFileAtPath:(NSString *)filePath attributes:(NSMutableDictionary *)attributes error:(NSError **)error;
+
+@end
